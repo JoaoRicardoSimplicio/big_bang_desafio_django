c58e3e02aa387ff225eda1737baba5773fd23304
João Ricardo <joaoricardosimplicio16@gmail.com>
2020-03-04 21:37:21 -0300

    Alterando nome de variaveis, funções e fazendo alguns comentários.

--- weather/urls.py
+++ weather/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.Initial, name='initial'),
-    path('consultar/', views.Consultar, name='consultar'),
+    path('consultar/', views.Consult, name='consult'),
     path('teste/', views.GetPlaylistSpotify, name='teste')
 ]
